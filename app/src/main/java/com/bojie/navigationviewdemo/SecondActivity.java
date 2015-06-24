ed1f81790ec45f3acefd062f9eb266b9618f2183
Bojie Jiang <jiangbojie@gmail.com>
2015-06-24 16:43:12 -0700

    TextInputLayout and snackbar

--- app/src/main/java/com/bojie/navigationviewdemo/SecondActivity.java
+++ app/src/main/java/com/bojie/navigationviewdemo/SecondActivity.java
@@ -1,16 +1,38 @@
 package com.bojie.navigationviewdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private RelativeLayout mRoot;
+    private TextInputLayout mInputLayout;
+    private TextInputLayout mPasswordLayout;
+    private EditText mInputEmail, mInputPassword;
+    private View.OnClickListener mSnackBarClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        mInputLayout = (TextInputLayout) findViewById(R.id.input_layout);
+        mPasswordLayout = (TextInputLayout) findViewById(R.id.input_password_layout);
+        mRoot = (RelativeLayout) findViewById(R.id.root_activity_second);
+        mInputEmail = (EditText) findViewById(R.id.input_email);
+        mInputPassword = (EditText) findViewById(R.id.input_password);
     }
 
     @Override
@@ -33,5 +55,34 @@ public class SecondActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void submit(View view) {
+        boolean isEmptyEmail = isEmptyEmail();
+        boolean isEmptyPassword = isEmptyPassword();
+
+        if (isEmptyEmail && isEmptyPassword) {
+            Snackbar.make(mRoot, "One or more fields are blank", Snackbar.LENGTH_SHORT)
+                    .setAction(getString(R.string.text_dismiss), mSnackBarClickListener)
+                    .show();
+        } else if (isEmptyEmail && !isEmptyPassword) {
+            mInputLayout.setError("Email cannot be blank");
+            mPasswordLayout.setError(null);
+        } else if (!isEmptyEmail && isEmptyPassword) {
+            mPasswordLayout.setError("Password cannot be blank");
+            mInputLayout.setError(null);
+        } else {
+            // Success
+        }
+    }
+
+    private boolean isEmptyEmail() {
+        return mInputEmail.getText() == null
+                || mInputEmail.getText().toString().isEmpty();
+    }
+
+    private boolean isEmptyPassword() {
+        return mInputPassword.getText() == null
+                || mInputPassword.getText().toString().isEmpty();
     }
 }
